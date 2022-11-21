ec84daf23dfb71faf2f27d8666a128c6fefcfb7d
flozo.home <github.mail@flozo.de>
2022-11-21 10:46:17 +0100

    Allow adding a new ingredient to a recipe

--- src/test/java/guru/springframework/controllers/IngredientControllerTest.java
+++ src/test/java/guru/springframework/controllers/IngredientControllerTest.java
@@ -76,6 +76,26 @@ class IngredientControllerTest {
     }
 
     @Test
+    public void testNewIngredientForm() throws Exception {
+        // given
+        RecipeCommand recipeCommand = new RecipeCommand();
+        recipeCommand.setId(1L);
+
+        // when
+        when(recipeService.findCommandById(anyLong())).thenReturn(recipeCommand);
+        when(unitOfMeasureService.listAllUoms()).thenReturn(new HashSet<>());
+
+        // then
+        mockMvc.perform(MockMvcRequestBuilders.get("/recipe/1/ingredient/new"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("recipe/ingredient/ingredientform"))
+                .andExpect(model().attributeExists("ingredient"))
+                .andExpect(model().attributeExists("uomList"));
+
+        verify(recipeService, times(1)).findCommandById(anyLong());
+
+    }
+    @Test
     public void testUpdateIngredientForm() throws Exception {
         // given
         IngredientCommand ingredientCommand = new IngredientCommand();
@@ -111,4 +131,5 @@ class IngredientControllerTest {
                 .andExpect(status().is3xxRedirection())
                 .andExpect(view().name("redirect:/recipe/2/ingredient/3/show"));
     }
+
 }
